aee83dd39d449db38a3cda68a6671c39b413edc5
AlexToop <alextoop@outlook.com>
2020-02-09 16:59:05 +0000

    started overwrite logic

--- bin/utils.js
+++ bin/utils.js
@@ -21,13 +21,28 @@ const getObjFromStdout = function (stdout) {
   return credentialsObject
 }
 
-const checkIfCredentialsAlreadyExist = async function (profile) {
-
-}
-
 const editAwsCredentials = async function (awsCredentialsDir, credentials, callback) {
-  await fs.appendFile(awsCredentialsDir, credentials)
-  return fs.readFile(awsCredentialsDir, 'utf8')
+  let currentFileContents = await fs.readFile(awsCredentialsDir, 'utf8')
+  const linesOfFile = currentFileContents.split(/\r?\n/)
+  const linesOfCredentials = credentials.split(/\r?\n/)
+  const profile = linesOfCredentials[0]
+
+  let i
+  let found = false
+  for (i = 0; i < linesOfFile.length; i++) {
+    if (linesOfFile[i] === profile) {
+      found = true
+      linesOfFile[i + 1] = linesOfCredentials[1]
+      linesOfFile[i + 2] = linesOfCredentials[2]
+      linesOfFile[i + 3] = linesOfCredentials[3]
+    }
+  }
+  currentFileContents = linesOfFile.join('\n')
+
+
+  let toWrite = (found) ? currentFileContents : currentFileContents + credentials
+  await fs.writeFile(awsCredentialsDir, toWrite)
+  return 
 }
 
 module.exports = {
